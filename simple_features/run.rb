2e7b896efde1edb184dc350250d2e39e5d599b70
Ysee Monnier <yseemonnier@gmail.com>
2018-01-20 22:44:33 +0100

    update simple features

--- simple_features/run.rb
+++ simple_features/run.rb
@@ -1,14 +1,44 @@
 require 'redis'
 
+def from_redis(m)
+    "REDIS: #{m}"
+end
 
 begin
-    redis = Redis.new(host: "192.168.99.100", port: 6379, db: 1)
-    redis.set("mykey", "hello world")
-    puts redis.get("mykey")
-rescue Exception => e
-  puts e.inspect
-  # => #<Redis::CannotConnectError: Timed out connecting to Redis on 10.0.1.1:6380>
+    puts 'Simple Features Ruby Script'
+    redis = Redis.new(host: "db", port: 6379, db: 1)
 
-  puts e.message
-  # => Timed out connecting to Redis on 10.0.1.1:6380
+    #
+    # GET SET
+    puts "--> GET / SET"
+    (a, b) = ['superkey', 'Super Value!']
+    puts "      set '#{a}' '#{b}'... "
+    redis.set(a, b)
+    puts "      #{from_redis("'#{a}' => #{redis.get(a)}")}"
+
+    #
+    # LIST
+    puts "--> LIST"
+    list = []
+    key_list = "superlist"
+    nb_element = 10
+    for i in 0..nb_element
+        value = "element #{i + 1}"
+        list.push(value)
+        puts "  add '#{value}' value to the list '#{key_list}'"
+        puts "      #{from_redis("LLEN #{key_list} => #{redis.llen(key_list)}")}"
+        redis.rpush(key_list, value)
+    end
+
+    puts "  get list '#{key_list}' from 0 to #{nb_element - 1} "
+    puts "      #{from_redis redis.lrange(key_list, 0, nb_element - 1)}"
+
+    for i in 0..nb_element
+        puts "  pop list '#{key_list}' ==> #{redis.lpop(key_list)}"
+        puts "      #{from_redis("LLEN #{key_list} => #{redis.llen(key_list)}")}"
+    end
+
+rescue Exception => e
+    puts e.inspect
+    puts e.message
 end
--- simple_features/run.sh
+++ simple_features/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # @author Ysee
-
+echo -e '************************\n'
+echo -e 'Simple features script...\n'
 bundle install
 ruby run.rb
+echo -e 'Simple feature script... DONE\n'
+echo -e '******************************\n'
